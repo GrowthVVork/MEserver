820a2dd56a17bc3411aec50cc8551b8ba848f1dd
Tejas <tejaskothari03@gmail.com>
2022-12-22 22:27:26 +0530

    Fixed type validation bug

--- invoiceReader/main.py
+++ invoiceReader/main.py
@@ -1,5 +1,17 @@
 # Invoice Reader for CLI operation
 import argparse
+import logging
+import logging.config
+import os
+from datetime import datetime
+# from dotenv import find_dotenv, load_dotenv
+
+# find .env file in parent directory
+# env_file = find_dotenv()
+# load_dotenv()
+
+CONFIG_DIR = "./config"
+LOG_DIR = "./logs"
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-src", "--source", help="Directory where invoices files exist.")
@@ -15,4 +27,18 @@ dest_dir = (args.destination)
 if not src_dir.exists():
     print("The source directory doesn't exist")
     raise SystemExit(1)
+
+def setup_logging():
+    """Load logging configuration"""
+    log_configs = {"dev": "logging.dev.ini", "prod": "logging.prod.ini"}
+    config = log_configs.get(os.environ["ENV"], "logging.dev.ini")
+    config_path = "/".join([CONFIG_DIR, config])
+
+    timestamp = datetime.now().strftime("%Y%m%d-%H:%M:%S")
+
+    logging.config.fileConfig(
+        config_path,
+        disable_existing_loggers=False,
+        defaults={"logfilename": f"{LOG_DIR}/{timestamp}.log"},
+    )
 
