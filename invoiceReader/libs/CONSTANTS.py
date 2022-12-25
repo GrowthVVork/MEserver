10cee7674dd06e1636d0406d3f65c1bda0979c11
imaeon <athulmohan4797@gmail.com>
2022-12-25 19:33:40 +0300

    added regex pattern for invoice

--- invoiceReader/libs/CONSTANTS.py
+++ invoiceReader/libs/CONSTANTS.py
@@ -1,5 +1,5 @@
 GTK_DLLS_ABS_PATH = r"C:\Program Files\GTK3-Runtime Win64\bin"
-# INVOICE_MATCHING_PATTERN = 
+INVOICE_MATCHING_PATTERN = r'SI+[/.-]\d+[/.-]\d+'
 from pathlib import Path
 mod_path = Path(__file__).parent.parent
 TEST_PDF_FILE = (mod_path / 'test/' / '37bad.pdf').resolve()
