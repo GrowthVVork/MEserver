42edff2ef3ddbe20511cdaff6e4791c734a97a83
imaeon <athulmohan4797@gmail.com>
2022-12-26 04:59:56 -0800

    Added aditional test files

--- invoiceReader/libs/CONSTANTS.py
+++ invoiceReader/libs/CONSTANTS.py
@@ -4,6 +4,8 @@ from pathlib import Path
 mod_path = Path(__file__).parent.parent
 TEST_PDF_FILE = (mod_path / 'test/' / '37bad.pdf').resolve()
 TEST_IMG_FILE = (mod_path / 'test/' / 'image_1.png').resolve()
+TEST_36badpng_FILE = (mod_path / 'test/' / '36bad.png').resolve()
 TEST_37badpng_FILE = (mod_path / 'test/' / '37bad.png').resolve()
+TEST_38badpng_FILE = (mod_path / 'test/' / '38bad.png').resolve()
 TEST_37bestpng_FILE = (mod_path / 'test/' / '37best.png').resolve()
 TEST_37bestpngcrop_FILE = (mod_path / 'test/' / '37bestcrop.png').resolve()
